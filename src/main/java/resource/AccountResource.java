fde93046750a3dcb13565dbbefdce75551fd7eab
s2793199 <a.a.tenhave@student.utwente.nl>
2022-06-10 12:18:03 +0200

    added functionality accountresource

--- src/main/java/resource/AccountResource.java
+++ src/main/java/resource/AccountResource.java
@@ -16,17 +16,32 @@ public class AccountResource {
 
     String username;
     String password;
-    public AccountResource(UriInfo uriInfo, Request request, String username, String password) {
-        this.uriInfo = uriInfo;
-        this.request = request;
-        this.username = username;
-        this.password = password;
-    }
+//    public AccountResource(UriInfo uriInfo, Request request, String username, String password) {
+//        this.uriInfo = uriInfo;
+//        this.request = request;
+//        this.username = username;
+//        this.password = password;
+//    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getAccountInfo() {
         JSONObject response = new JSONObject();
+        MultivaluedMap<String, String> parameters = uriInfo.getQueryParameters();
+        String email = "";
+        String password = "";
+        if (parameters.containsKey("email")) {
+            email = parameters.get("email").get(0);
+        }
+        if (parameters.containsKey("password")) {
+            password = parameters.get("password").get(0);
+        }
+        System.out.println("test");
+        System.out.println(email);
+        System.out.println(password);
+        String query = "SELECT password\n" +
+                "FROM Test.account\n" +
+                "WHERE username =" + username + " AND password =" + password;
         //Account account = jsonStringToCar(Dao.getAccount(username, password));
         return response.toString();
     }
