78adf7031f2bc8e9d6747af2fadf3e317ad3996e
mooon53 <katherinrad2409@gmail.com>
2022-06-22 10:21:50 +0200

    classes for checkout process [filled]

--- src/main/java/resource/CheckoutResource.java
+++ src/main/java/resource/CheckoutResource.java
@@ -1,4 +1,39 @@
 package resource;
 
+import dao.CheckoutDao;
+import model.CarOrder;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("checkout")
 public class CheckoutResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void createCart(List<CarOrder> input){
+        for(CarOrder item : input){
+            CheckoutDao.instance.addItem(item);
+            System.out.println(item+"\n");
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CarOrder> getCart() {return CheckoutDao.instance.getModel();}
+
+    @DELETE
+    public void deleteShoppingCart(){CheckoutDao.instance.getModel().clear();}
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public double total(){
+        List<CarOrder> cars = CheckoutDao.instance.getModel();
+        double cost = 0;
+        for(CarOrder item : cars){
+            cost += 1;
+        }
+        return  cost;
+    }
 }
