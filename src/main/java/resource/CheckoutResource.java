9881cf4a867101d0007e0678b6c39b0bed2b8043
mooon53 <katherinrad2409@gmail.com>
2022-06-23 09:16:31 +0200

    smth

--- src/main/java/resource/CheckoutResource.java
+++ src/main/java/resource/CheckoutResource.java
@@ -1,7 +1,9 @@
 package resource;
 
-import dao.CheckoutDao;
+import dao.CartDao;
 import model.CarOrder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,24 +14,28 @@ public class CheckoutResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void createCart(List<CarOrder> input){
-        for(CarOrder item : input){
-            CheckoutDao.instance.addItem(item);
+    public void addToCart(String input){
+        JSONObject JSON = new JSONObject(input);
+        JSONArray options = JSON.getJSONArray("chosenOptions");
+        for (Object i : options) {
+            int item = (Integer) i;
+            //TODO: add items to cart
             System.out.println(item+"\n");
         }
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<CarOrder> getCart() {return CheckoutDao.instance.getModel();}
+    public List<CarOrder> getCart() {return CartDao.instance.getModel();}
 
     @DELETE
-    public void deleteShoppingCart(){CheckoutDao.instance.getModel().clear();}
+    public void deleteShoppingCart(){
+        CartDao.instance.getModel().clear();}
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public double total(){
-        List<CarOrder> cars = CheckoutDao.instance.getModel();
+        List<CarOrder> cars = CartDao.instance.getModel();
         double cost = 0;
         for(CarOrder item : cars){
             cost += 1;
