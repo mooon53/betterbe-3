5e6c8f1aabe877792fb4aa6a9614cbb0ba433f4c
Dani <d.mahaini@student.utwente.nl>
2022-06-23 12:04:41 +0200

    Added email showcase in the account details page

--- src/main/webapp/accountDetails.js
+++ src/main/webapp/accountDetails.js
@@ -1,3 +1,6 @@
+let sessionID = getSessionId();
+let acc;
+let responsse;
 function onLoad() {
 	sessionId();
 }
@@ -5,10 +8,22 @@ function onLoad() {
 function addCarPage(){
 	location.href = "addCar.html"
 }
-function historyPage(){
-	location.href = "timeline.html"
-}
 
 function displayEmail(){
+	let request = new XMLHttpRequest();
+	request.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			responsse = this.response;
+			acc = JSON.parse(responsse);
 
+		}
+	}
+	request.open("GET", "http://localhost:8080/betterbe_3/rest/sessions/" + sessionID, false);
+	request.send();
+	if(sessionValid()){
+		console.log(responsse);
+		let email = acc.account.email;
+		console.log(email);
+		document.getElementById("emailDisplay").innerHTML = email;
+	}
 }
