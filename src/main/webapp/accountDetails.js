c054befc4f9e04018e4f55eef6a469f8feb27a33
Dani <d.mahaini@student.utwente.nl>
2022-06-24 09:43:16 +0200

    The account details page now looks different for customer and employee accounts

--- src/main/webapp/accountDetails.js
+++ src/main/webapp/accountDetails.js
@@ -6,7 +6,10 @@ function onLoad() {
 }
 
 function addCarPage(){
-	location.href = "addCar.html"
+	location.href = "addCar.html";
+}
+function checkoutPage(){
+	location.href = "checkout.html";
 }
 
 function displayEmail(){
@@ -15,7 +18,6 @@ function displayEmail(){
 		if (this.readyState === 4 && this.status === 200) {
 			responsse = this.response;
 			acc = JSON.parse(responsse);
-
 		}
 	}
 	request.open("GET", "http://localhost:8080/betterbe_3/rest/sessions/" + sessionID, false);
@@ -25,5 +27,18 @@ function displayEmail(){
 		let email = acc.account.email;
 		console.log(email);
 		document.getElementById("emailDisplay").innerHTML = email;
+	}
+	document.getElementById("moveToAdd").style.visibility = 'hidden';
+	document.getElementById("moveToCheckout").style.visibility = 'hidden';
+
+}
+
+function empCheck(){
+	if(employeeCheck()){
+		document.getElementById("moveToAdd").style.visibility = 'visible';
+	}
+	else{
+		document.getElementById("moveToCheckout").style.visibility = 'visible';
+
 	}
 }
